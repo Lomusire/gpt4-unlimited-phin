43bea416e62528fc34b08b5caa9d10771d8b17f8
Scipline <xiaozong_52@qq.com>
2023-11-13 09:09:55 +0800

    1. Remove unnecessary dark styles, Phind has a dark mode in the upper right corner 2. Fix the permissions in the manifest 3. Optimize the refresh token logic in content.js

--- content.js
+++ content.js
@@ -1,26 +1,20 @@
-let eventCount = 0;
-
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function (e) {
   if (e.key === 'Enter') {
-    eventCount++;
-    checkEventCount();
+    checkEventCount()
   }
-});
-
-document.addEventListener('click', function() {
-  eventCount++;
-  checkEventCount();
-});
-
+})
+setInterval(function () {
+  checkEventCount()
+}, 5000)
 function checkEventCount() {
-  if (eventCount >= 10) {
-    chrome.runtime.sendMessage({type: 'fetchToken'});
-    eventCount = 0;
+  const left = +document.querySelector('.text-primary').firstChild.textContent
+  if (left <= 0) {
+    chrome.runtime.sendMessage({ type: 'fetchToken' })
   }
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'reloadPage') {
-    window.location.reload();
+    window.location.reload()
   }
-});
+})
