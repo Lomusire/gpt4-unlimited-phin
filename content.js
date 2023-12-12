0c5e42d4e5dcb0b9f98b88c4aec6a9939413b55e
Scipline <Scipline52@gmail.com>
2023-12-13 00:06:09 +0800

    Fix some bugs;Enhanced judgment logic

--- content.js
+++ content.js
@@ -1,3 +1,11 @@
+/**
+ * @Author        Lomusire
+ * @Since         2023-11-07 03:40:37
+ * @LastEditor    Scipline
+ * @LastEditTime  2023-12-12 23:57:41
+ * @FileName      content.js
+ * @Description
+ */
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Enter') {
     checkEventCount()
@@ -6,12 +14,40 @@ document.addEventListener('keydown', function (e) {
 setInterval(function () {
   checkEventCount()
 }, 5000)
-function checkEventCount() {
-  const left = +document.querySelector('.text-primary').firstChild.textContent
-  if (left <= 0) {
-    chrome.runtime.sendMessage({ type: 'fetchToken' })
+
+function checkEventCount () {
+  //Resolve the issue of GPT-4 quota misjudgment in the new version of phind, even when not logged in
+  const logStatus = document.querySelector('.btn.btn-sm').textContent
+  chrome.runtime.sendMessage(
+    {
+      action: "getToken",
+      domain: "https://www.phind.com",
+      field: "__Secure-next-auth.session-token",
+    },
+    (res) => {
+      if (!res || !res.cookie || !res.cookie.value || logStatus === 'Sign In') {
+        //console.error('No value found for Token');
+        chrome.runtime.sendMessage({ type: 'fetchToken' });
+      }
+
+    }
+  );
+
+  const textPrimaryElement = document.querySelector('.text-primary');
+  //When chatting, can't find the determining elements
+  if (textPrimaryElement && textPrimaryElement.firstChild) {
+    const left = +textPrimaryElement.firstChild.textContent;
+
+    if (isNaN(left) || left <= 0) {
+      chrome.runtime.sendMessage({ type: 'fetchToken' });
+    }
+
   }
+
 }
+
+
+
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'reloadPage') {
